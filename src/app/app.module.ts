9e5f7ed94aa6a048718730b1558c98addf06c678
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2019-02-25 16:30:25 +0530

    seed project

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BadgeComponent } from './badge.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, BadgeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
